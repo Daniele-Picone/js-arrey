5042a4911d0f5f69013ec7fcc2d6634d05ed5506
Daniele-Picone <danielepicone97@gmail.com>
2024-09-11 12:19:19 +0200

    correzione selfowork arrey1

--- arrey1.js
+++ arrey1.js
@@ -3,5 +3,9 @@
 let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25];
 console.log(numbers);
 
-let order = numbers.sort( (a,b) => a - b  );
-console.log(order);
+let crescente = numbers.sort( (a , b) => a - b  );
+console.log(crescente);
+
+let decrescente = numbers.sort(  (a , b) => b - a );
+console.log(decrescente);
+
